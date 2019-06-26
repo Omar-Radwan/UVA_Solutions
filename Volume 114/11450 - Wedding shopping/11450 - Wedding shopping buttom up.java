e686a4d5403b2079ff49d3bdd1549109288a9ab0
Omar Radwan <omarrm1999@gmail.com>
2019-06-26 13:04:27 +0200

    a

---
+++ Volume 114/11450 - Wedding shopping/11450 - Wedding shopping buttom up.java
@@ -0,0 +1,104 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static FastReader input = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+    static final int _IINF = (int) (-1e9 - 20);
+
+    /*
+    * 
+    */
+
+    static int[][] a;
+    static boolean[][] v;
+
+    public static void main(String[] args) throws IOException {
+        int n = input.nextInt();
+        while (n-- > 0) {
+            int m = input.nextInt();
+            int c = input.nextInt();
+            
+            a = new int[c][];
+            v = new boolean[c + 5][m + 5];
+            
+            for (int i = 0; i < c; i++) {
+
+                a[i] = new int[input.nextInt()];
+
+                for (int j = 0; j < a[i].length; j++)
+                    a[i][j] = input.nextInt();
+            }
+            
+    
+
+            for (int i = 0; i < a[0].length; i++) {
+                int p = a[0][i];
+                if (m - p >= 0)
+                    v[0][m - p] = true;
+            }
+
+            for (int i = 1; i < a.length; i++) {
+                for (int k = 0; k < a[i].length; k++) {
+                    int p = a[i][k];
+                    for (int j = 0; j <= m; j++) {
+                        if (v[i - 1][j] && j - p >= 0) {
+                            v[i][j - p] = true;
+                        }
+                    }
+                }
+            }
+            
+            int best = -1;
+            
+            for (int i = 0; i <= m && best == -1; i++) {
+                if (v[c - 1][i])
+                    best = i;
+            }
+
+            out.println(best >= 0 ? m - best : "no solution");
+        }
+
+        out.flush();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws NumberFormatException, IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws NumberFormatException, IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws NumberFormatException, IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            String str = "";
+            str = br.readLine();
+            return str;
+        }
+    }
+
+}
